1d888924c6a936ed815047d9f8725ed9eb7a4195
evaporator <osxar.jeavons@gmail.com>
2026-04-05 22:09:28 +0100

    Reorganised project and added basic test harness

---
+++ src/Tests.rs
@@ -0,0 +1,443 @@
+#[cfg(test)]
+mod tests {
+    use crate::app;
+    use axum_test::TestServer;
+
+    fn server() -> TestServer {
+        TestServer::new(app("generate"))
+    }
+
+    fn query_builder(pairs: &[(&str, Option<&str>)]) -> String {
+        pairs
+            .iter()
+            .filter_map(|(k, v)| v.map(|val| format!("{}={}", k, val)))
+            .collect::<Vec<_>>()
+            .join("&")
+    }
+
+    // -----------------------------------------------------------------------
+    // Passing tests
+    // -----------------------------------------------------------------------
+
+    #[tokio::test]
+    async fn test_png_Ellers() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("Weighting", None),
+            ("SAlgorithm", None),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/png");
+        assert!(!res.as_bytes().is_empty(), "PNG body must not be empty");
+    }
+
+    #[tokio::test]
+    async fn test_png_GrowingTree() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("15")),
+            ("Height", Some("15")),
+            ("Algorithm", Some("g")),
+            ("Weighting", Some("0.5")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", None),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/png");
+    }
+
+    #[tokio::test]
+    async fn test_jpg_format() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("jpg")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/jpg");
+    }
+
+    #[tokio::test]
+    async fn test_jpeg_format() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("jpeg")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/jpg");
+    }
+
+    #[tokio::test]
+    async fn test_bmp_format() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("bmp")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/bmp");
+    }
+
+    #[tokio::test]
+    async fn test_webp_format() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("webp")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/webp");
+    }
+
+    #[tokio::test]
+    async fn test_tga_format() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("tga")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/tga");
+    }
+
+    #[tokio::test]
+    async fn test_ico_format() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("1")),
+            ("Height", Some("1")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("ico")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/ico");
+    }
+
+    #[tokio::test]
+    async fn test_dijkstra() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("d")),
+            ("StartX", Some("0")),
+            ("StartY", Some("0")),
+            ("EndX", Some("9")),
+            ("EndY", Some("9")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/png");
+    }
+
+    #[tokio::test]
+    async fn test_astar() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("a")),
+            ("StartX", Some("0")),
+            ("StartY", Some("0")),
+            ("EndX", Some("9")),
+            ("EndY", Some("9")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+        assert_eq!(res.headers()["content-type"], "image/png");
+    }
+
+    #[tokio::test]
+    async fn test_max_distance() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("20")),
+            ("Height", Some("20")),
+            ("Algorithm", Some("g")),
+            ("Weighting", Some("0.7")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("a")),
+            ("StartX", Some("0")),
+            ("StartY", Some("0")),
+            ("EndX", Some("19")),
+            ("EndY", Some("19")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+    }
+
+    #[tokio::test]
+    async fn test_dimensions_min() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("1")),
+            ("Height", Some("1")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+    }
+
+    #[tokio::test]
+    async fn test_dimensions_max() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("1000")),
+            ("Height", Some("1000")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+    }
+
+    #[tokio::test]
+    async fn test_no_path() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("d")),
+            ("StartX", Some("2")),
+            ("StartY", Some("2")),
+            ("EndX", Some("2")),
+            ("EndY", Some("2")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+    }
+
+    #[tokio::test]
+    async fn test_0_weighting() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("g")),
+            ("Weighting", Some("0.0")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+    }
+
+    #[tokio::test]
+    async fn test_1_weighting() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("g")),
+            ("Weighting", Some("1.0")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_ok();
+    }
+
+    // -----------------------------------------------------------------------
+    // Exception tests
+    // -----------------------------------------------------------------------
+
+    #[tokio::test]
+    async fn test_width_over_limit() {
+        // width cap is 1000
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("1001")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_height_over_limit() {
+        //height cap is 1000
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("1001")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_unsupported_extension() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("svg")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_invalid_algorithm() {
+        // supported algorithms are Ellers (e) or Growing Tree (g)
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("z")),
+            ("Extension", Some("png")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_no_weighting() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("10")),
+            ("Height", Some("10")),
+            ("Algorithm", Some("g")),
+            ("Extension", Some("png")),
+            ("Weighting", None),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_invalid_solver_algorithm() {
+        // supported algorithms are Dijkstra's and A*
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("z")),
+            ("StartX", Some("0")),
+            ("StartY", Some("0")),
+            ("EndX", Some("4")),
+            ("EndY", Some("4")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_solver_out_of_bounds() {
+        // max cords are (width -1, height -1)
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("d")),
+            ("StartX", Some("0")),
+            ("StartY", Some("0")),
+            ("EndX", Some("99")),
+            ("EndY", Some("99")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_solver_missing_start_coords() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("d")),
+            ("StartX", None),
+            ("StartY", None),
+            ("EndX", Some("4")),
+            ("EndY", Some("4")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_solver_missing_end_coords() {
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("5")),
+            ("Height", Some("5")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("png")),
+            ("SAlgorithm", Some("d")),
+            ("StartX", Some("0")),
+            ("StartY", Some("0")),
+            ("EndX", None),
+            ("EndY", None),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_ico_dimensions_over_limit() {
+        // ICO max is 256x256 px. 13x13 = 262 px.
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("13")),
+            ("Height", Some("13")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("ico")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+
+    #[tokio::test]
+    async fn test_webp_dimensions_over_limit() {
+        // WebP max is 16383x16383. 820x820 = 16402 px.
+        let server = server();
+        let q = query_builder(&[
+            ("Width", Some("820")),
+            ("Height", Some("820")),
+            ("Algorithm", Some("e")),
+            ("Extension", Some("webp")),
+        ]);
+        let res = server.get(&format!("/generate?{}", q)).await;
+        res.assert_status_bad_request();
+    }
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,234 @@
+use MazeGenerator::{
+    Draw::{grid_draw_img, solve_draw_img},
+    Generator::{Ellers, Growing_Tree},
+    Maze::Grid::Grid,
+    Solver::{Astar, dijkstra},
+};
+use axum::{Router, extract::Query, http::StatusCode, response::IntoResponse, routing::get};
+use image::{
+    EncodableLayout, ExtendedColorType, ImageEncoder,
+    codecs::{
+        bmp::BmpEncoder, gif::GifEncoder, ico::IcoEncoder, jpeg::JpegEncoder, png::PngEncoder,
+        qoi::QoiEncoder, tga::TgaEncoder, webp::WebPEncoder,
+    },
+};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct MazeParameters {
+    Width: usize,
+    Height: usize,
+    Algorithm: char,
+    Weighting: Option<f32>,
+    SAlgorithm: Option<char>,
+    StartX: Option<usize>,
+    StartY: Option<usize>,
+    EndX: Option<usize>,
+    EndY: Option<usize>,
+    Extension: String,
+}
+
+mod Tests;
+
+pub fn app(path: &str) -> Router {
+    Router::new().route(format!("/{}", path).as_str(), get(handler))
+}
+
+pub async fn handler(
+    Query(params): Query<MazeParameters>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let img_extensions = [
+        "png", "jpg", "jpeg", "bmp", "gif", "ico", "webp", "tga", "qoi",
+    ];
+
+    if params.Width > 1000 || params.Height > 1000 {
+        return Err((StatusCode::BAD_REQUEST, "Invalid dimensions".to_string()));
+    } else if !img_extensions.contains(&params.Extension.as_str()) {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            "Unsupported file extension".to_string(),
+        ));
+    } else if validate_dimensions(params.Width, params.Height, &params.Extension) {
+        return Err((
+            StatusCode::BAD_REQUEST,
+            "Dimensions exceeed file format limits".to_string(),
+        ));
+    }
+
+    let maze = match params.Algorithm {
+        'e' => Ellers(Grid::new(params.Width, params.Height)),
+        'g' => match params.Weighting {
+            Some(w) => Growing_Tree(Grid::new(params.Width, params.Height), w),
+            None => {
+                return Err((
+                    (StatusCode::BAD_REQUEST),
+                    "Weighting required for Growing_Tree Algorithm".to_string(),
+                ));
+            }
+        },
+        _ => {
+            return Err((StatusCode::BAD_REQUEST, "Invalid Algorithm".to_string()));
+        }
+    };
+
+    let Solution: Option<Vec<[usize; 2]>> = match params.SAlgorithm {
+        Some(a) => {
+            match ValidCoords(
+                &maze,
+                params.StartX,
+                params.StartY,
+                params.EndX,
+                params.EndY,
+            ) {
+                Ok(()) => Some(match a {
+                    'd' => dijkstra(
+                        [params.StartX.unwrap(), params.StartY.unwrap()],
+                        [params.EndX.unwrap(), params.EndY.unwrap()],
+                        &maze,
+                    ),
+                    'a' => Astar(
+                        [params.StartX.unwrap(), params.StartY.unwrap()],
+                        [params.EndX.unwrap(), params.EndY.unwrap()],
+                        &maze,
+                    ),
+                    _ => {
+                        return Err((
+                            StatusCode::BAD_REQUEST,
+                            "Invalid Solution Algorithm".to_string(),
+                        ));
+                    }
+                }),
+                Err(e) => return Err((StatusCode::BAD_REQUEST, e)),
+            }
+        }
+        None => None,
+    };
+
+    let (img_bytes, colour_type, height, width) = match Solution {
+        Some(s) => {
+            let img = solve_draw_img(&maze, &s);
+            (
+                img.as_bytes().to_vec(),
+                ExtendedColorType::Rgb8,
+                img.height(),
+                img.width(),
+            )
+        }
+        None => {
+            let img = grid_draw_img(&maze);
+            (
+                img.as_bytes().to_vec(),
+                ExtendedColorType::L8,
+                img.height(),
+                img.width(),
+            )
+        }
+    };
+
+    let mut buffer = Vec::new();
+
+    match params.Extension.as_str() {
+        "png" => {
+            let encoder = PngEncoder::new(&mut buffer);
+            encoder
+                .write_image(&img_bytes, width, height, colour_type)
+                .unwrap();
+
+            Ok((StatusCode::OK, [("Content-Type", "image/png")], buffer))
+        }
+        "jpg" | "jpeg" => {
+            let encoder = JpegEncoder::new(&mut buffer);
+            encoder
+                .write_image(&img_bytes, width, height, colour_type)
+                .unwrap();
+
+            Ok((StatusCode::OK, [("Content-Type", "image/jpg")], buffer))
+        }
+        "bmp" => {
+            let encoder = BmpEncoder::new(&mut buffer);
+            encoder
+                .write_image(&img_bytes, width, height, colour_type)
+                .unwrap();
+
+            Ok((StatusCode::OK, [("Content-Type", "image/bmp")], buffer))
+        }
+        "webp" => {
+            let encoder = WebPEncoder::new_lossless(&mut buffer);
+            encoder
+                .write_image(&img_bytes, width, height, colour_type)
+                .unwrap();
+
+            Ok((StatusCode::OK, [("Content-Type", "image/webp")], buffer))
+        }
+        "ico" => {
+            let encoder = IcoEncoder::new(&mut buffer);
+            encoder
+                .write_image(&img_bytes, width, height, colour_type)
+                .unwrap();
+
+            Ok((StatusCode::OK, [("Content-Type", "image/ico")], buffer))
+        }
+        "tga" => {
+            let encoder = TgaEncoder::new(&mut buffer);
+            encoder
+                .write_image(&img_bytes, width, height, colour_type)
+                .unwrap();
+
+            Ok((StatusCode::OK, [("Content-Type", "image/tga")], buffer))
+        }
+        _ => return Err((StatusCode::BAD_REQUEST, "Encoding failed".to_string())),
+    }
+}
+
+fn ValidCoords(
+    grid: &Grid,
+    x1: Option<usize>,
+    y1: Option<usize>,
+    x2: Option<usize>,
+    y2: Option<usize>,
+) -> Result<(), String> {
+    let max_x = grid.width - 1;
+    let max_y = grid.height - 1;
+
+    let check = |x: Option<usize>, y: Option<usize>| -> Result<(), String> {
+        match (x, y) {
+            (Some(x_val), Some(y_val)) => {
+                if x_val > max_x || y_val > max_y {
+                    return Err("Coordinates must be within bounds".to_string());
+                }
+                Ok(())
+            }
+            (None, _) | (_, None) => Err("Coordinates must be present for Solution".to_string()),
+        }
+    };
+
+    check(x1, y1)?;
+    check(x2, y2)?;
+
+    Ok(())
+}
+
+fn get_format_limits(extension: &str) -> Option<(u32, u32)> {
+    match extension {
+        "png" => Some((u32::MAX, u32::MAX)),
+        "jpg" | "jpeg" => Some((65535, 65535)),
+        "webp" => Some((16383, 16383)),
+        "bmp" => Some((u32::MAX, u32::MAX)),
+        "gif" => Some((65535, 65535)),
+        "ico" => Some((256, 256)),
+        "tga" => Some((65535, 65535)),
+        "qoi" => Some((u32::MAX, u32::MAX)),
+        _ => None,
+    }
+}
+
+fn validate_dimensions(width: usize, height: usize, extension: &str) -> bool {
+    let img_width = (width * 20 + 2 * 1) as u32;
+    let img_height = (height * 20 + 2 * 1) as u32;
+    let limits = get_format_limits(extension).unwrap();
+
+    if limits.0 < img_width || limits.1 < img_height {
+        return true;
+    }
+    false
+}
